d3183b7ad4b352182d11a0e30304686dfea86d58
Sunghun Kim <sunghunkim@Sunghunui-MacBook-Air.local>
2020-07-01 17:11:45 +0900

    lecture 6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+
+  
+})
 
 const customers = [
 {
@@ -30,11 +42,24 @@ const customers = [
 }
 ]
 
-function App() {
+class App extends Component{
+  render(){
+    const { classes } = this.props; 
   return (
-    <div>
-      {
-        customers.map(c => {
+    <Paper className = {classes.root}>
+      <Table className = {classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {customers.map(c => {
           return(
             <Customer
               key={c.id}
@@ -46,11 +71,15 @@ function App() {
               job = {c.job}
             />
           );
-        })
-      }
-    </div>
+         })
+       } 
+        </TableBody>
+      </Table>
+
+    </Paper>
     
   );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
