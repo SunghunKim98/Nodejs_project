bed1ead6d1e33b3b6124c0a44e761bd973b760a8
Sunghun Kim <sunghunkim@Sunghunui-MacBook-Air.local>
2020-07-09 17:51:37 +0900

    Add progress bar

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 const styles = theme => ({
 
@@ -19,16 +21,21 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 })
 
 class App extends Component{
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress,20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -40,6 +47,11 @@ class App extends Component{
     return body;
   }
 
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed: completed>=100 ? 0 : completed+1})
+
+  }
 
   render(){
     const { classes } = this.props; 
@@ -69,7 +81,12 @@ class App extends Component{
               job = {c.job}
             />
           );
-         }) : ""
+         }) : 
+         <TableRow>
+           <TableCell colSpan="6" align="center">
+               <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}></CircularProgress>
+           </TableCell>
+         </TableRow>
        } 
         </TableBody>
       </Table>
