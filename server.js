71df011457feca554e9b992828d77feaf131fe1a
Sunghun Kim <sunghunkim@Sunghunui-MacBook-Air.local>
2020-07-15 14:15:31 +0900

    Create Customer Database Table & Node.js Database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,35 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+	host: conf.host,
+	user: conf.user,
+	password: conf.password,
+	port: conf.port,
+	database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req,res) => { 
-    res.send([
-    
-    {
-		"id": 1,
-		"image": "http://placeimg.com/64/64/1",
-		"name": "김성훈남간지",
-		"birthday": "981210",
-		"gender": "남자",
-		"job": "학생"
-	},
-	{
-		"id": 2,
-		"image": "http://placeimg.com/64/64/2",
-		"name": "나는야훈남간지",
-		"birthday": "971110",
-		"gender": "남자",
-		"job": "프로그래머"
-	},
-	{
-		"id": 3,
-		"image": "http://placeimg.com/64/64/3",
-		"name": "김지훈",
-		"birthday": "980810",
-		"gender": "여자",
-		"job": "대학생"
-	}
-]
-    )
+    connection.query(
+		"select * from CUSTOMER",
+		(err,rows,fields) => {
+				res.send(rows);
+		}
+	);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
